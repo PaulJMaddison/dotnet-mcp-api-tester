eb58b190cec82e01d0d0a45ff71fda3a5e8e0519
Paul Maddison <paul.maddison.delimeg@gmail.com>
2025-12-21 12:46:05 +0000

    Day 6, add api_reset_policy tool and tidy client runtime state after SSRF test

---
+++ ApiTester.McpServer/Services/ApiPolicyPresets.cs
@@ -0,0 +1,28 @@
+﻿namespace ApiTester.McpServer.Services;
+
+public static class ApiPolicyPresets
+{
+    public static ApiExecutionPolicy SafeDefaults() => new()
+    {
+        DryRun = true,
+        AllowedBaseUrls = new(),
+        AllowedMethods = new() { "GET" },
+        BlockLocalhost = true,
+        BlockPrivateNetworks = true,
+        Timeout = TimeSpan.FromSeconds(10),
+        MaxRequestBodyBytes = 262_144,
+        MaxResponseBodyBytes = 524_288
+    };
+
+    public static ApiExecutionPolicy AllowPetstore() => new()
+    {
+        DryRun = false,
+        AllowedBaseUrls = new() { "https://petstore3.swagger.io/api/v3" },
+        AllowedMethods = new() { "GET" },
+        BlockLocalhost = true,
+        BlockPrivateNetworks = true,
+        Timeout = TimeSpan.FromSeconds(10),
+        MaxRequestBodyBytes = 262_144,
+        MaxResponseBodyBytes = 524_288
+    };
+}
