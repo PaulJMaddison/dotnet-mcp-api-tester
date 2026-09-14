e77be96465f6995e3c86c36a8139f2b837b1c7f1
Paul Maddison <paul.maddison.delimeg@gmail.com>
2026-09-14 14:38:05 +0100

    Leave Azure completion limits opt-in

--- ApiTester.McpServer/Program.cs
+++ ApiTester.McpServer/Program.cs
@@ -49,7 +49,7 @@ var azureOpenAi = new AzureOpenAiOptions
     MaxRetries = builder.Configuration.GetValue<int?>("AzureOpenAI:MaxRetries") ?? 2,
     MaxResponseBytes = builder.Configuration.GetValue<int?>("AzureOpenAI:MaxResponseBytes") ?? 1_048_576,
     MaxInputChars = builder.Configuration.GetValue<int?>("AzureOpenAI:MaxInputChars") ?? 120_000,
-    MaxCompletionTokens = builder.Configuration.GetValue<int?>("AzureOpenAI:MaxCompletionTokens") ?? 1_500,
+    MaxCompletionTokens = builder.Configuration.GetValue<int?>("AzureOpenAI:MaxCompletionTokens") ?? 0,
     CircuitBreakerFailureThreshold = builder.Configuration.GetValue<int?>("AzureOpenAI:CircuitBreakerFailureThreshold") ?? 4,
     CircuitBreakerBreakSeconds = builder.Configuration.GetValue<int?>("AzureOpenAI:CircuitBreakerBreakSeconds") ?? 30
 };
