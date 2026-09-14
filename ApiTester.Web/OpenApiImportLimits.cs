4e29a4950366faa4828bf9b0c6459fabc7979344
Paul Maddison <paul.maddison.delimeg@gmail.com>
2026-09-14 17:35:23 +0100

    Align Web OpenAPI import limit with MCP hardening

--- ApiTester.Web/OpenApiImportLimits.cs
+++ ApiTester.Web/OpenApiImportLimits.cs
@@ -2,6 +2,6 @@ namespace ApiTester.Web;
 
 public static class OpenApiImportLimits
 {
-    public const int MaxSpecBytes = 1_000_000;
+    public const int MaxSpecBytes = 16 * 1024 * 1024;
     public const int MaxRequestBodyBytes = MaxSpecBytes + 16_384;
 }
